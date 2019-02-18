7fb69abea326887b4cb5caf3367c9fb368680a8c
Saxon Jensen <saxon.jensen@healthengine.com.au>
2019-02-18 13:30:05 +0800

    Basic facelet model. Still working out moves.

--- src/FaceModel/Cube.hs
+++ src/FaceModel/Cube.hs
@@ -5,10 +5,35 @@ import Prelude hiding (Right, Left)
 data Face = Front | Right | Back | Left | Up | Down deriving (Eq, Ord, Enum, Show)
 data Colour = Green | Red | Orange | Blue | Yellow | White deriving (Eq, Enum, Ord, Show)
 
-type Facelet = (Face, Color)
+instance Show Colour where
+    show c = "\x1b[" ++ (colourCode c) ++ "m\x1b[0m"
+    where
+        colourCode Green = "32"
+        colourCode Red = "31"
+        colourCode Orange = "35"
+        colourCode Blue = "34"
+        colourCode Yellow = "33"
+        colourCode White = "37"
+
+type Facelet = (Face, Colour)
 
 type Cublet = [Facelet]
 
 type Cube = [Cublet]
 
 data Rotation = Clockwise | Anticlockwise deriving (Eq, Enum, Ord, Show)
+
+faceletFace :: Facelet -> Face
+faceletFace = fst
+
+faceletColour :: Facelet -> Colour
+faceletColour = snd
+
+correctFacelet :: Facelet -> Face
+correctFacelet = toEnum . fromEnum . faceletColour
+
+isFaceletCorrect :: Facelet -> Bool
+isFaceletCorrect f = faceletFace f == correctFacelet f
+
+isCubeletCorrect :: Cublet -> Bool
+isCubeletCorrect = all isFaceletCorrect
